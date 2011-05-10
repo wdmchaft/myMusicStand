a2f54cce7454d2116bf9188febd359a4cd1200d4
Steven Solomon <solomon.steven.m@gmail.com>
2011-05-10 13:59:39 -0400

    Add test to confirm core data is setup, and to provide in memory persistent store for model testing

---
+++ myMusicStandLogicTests/CoreDataTest.h
@@ -0,0 +1,26 @@
+//
+//  CoreDataTests.h
+//  PDFPrototyping
+//  
+//  This SenTestCase subclass is to be used to make testing of 
+//  CoreData functionality easier by wrapping the setUp/tearDown
+//  needed into a convient class. 
+//
+//  PLEASE REMEMBER TO INCLUDE THE COREDATA MODEL FILE YOU WANT TO
+//  TEST IN THE COPY BUNDLE RESOURCE FOR THAT TARGET CONTAINING THIS
+//  ALL TESTS THAT SUBCLASS THIS ONE. enjoy and sorry for yelling =)
+//
+//  Created by Steve Solomon on 5/6/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface CoreDataTest : SenTestCase {
+    NSManagedObjectModel *model;
+    NSPersistentStoreCoordinator *coordinator;
+    NSManagedObjectContext *context;
+}
+
+@end
---
+++ myMusicStandLogicTests/CoreDataTest.m
@@ -0,0 +1,47 @@
+//
+//  CoreDataTests.m
+//  PDFPrototyping
+//
+//  Created by Steve Solomon on 5/6/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "CoreDataTest.h"
+#import <CoreData/CoreData.h>
+
+@implementation CoreDataTest
+
+- (void)setUp
+{
+    [super setUp];
+    // Get the bundle for this class 
+    //(the datamodel must be in the copy bundle resources for the target containing the tests)
+    NSBundle * bundle = [NSBundle bundleForClass:[self class]];
+    model = [NSManagedObjectModel mergedModelFromBundles:[NSArray arrayWithObject:bundle]];
+    coordinator = [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel:model];
+    // Add an in memory persistent store, so we can test saving operations
+    [coordinator addPersistentStoreWithType:NSInMemoryStoreType
+                              configuration:nil 
+                                        URL:nil
+                                    options:nil
+                                      error:nil];
+    context = [[NSManagedObjectContext alloc] init];
+    [context setPersistentStoreCoordinator:coordinator];
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    [context release];
+    [coordinator release];
+    [super tearDown];
+}
+
+- (void)testManagedObjectContextSetup
+{
+    STAssertNotNil(coordinator, @"The coordinator shouldn't be nil");
+    STAssertNotNil(context, @"The context shouldn't be nil");
+    
+}
+
+@end
