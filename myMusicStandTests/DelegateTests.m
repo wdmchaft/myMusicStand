6c7dd710bce8c7f74b9be852388803efbcccc620
Steven Solomon <solomon.steven.m@gmail.com>
2011-05-12 11:04:47 -0400

    Rename SetupTests to be DelegateTests

---
+++ myMusicStandTests/DelegateTests.m
@@ -0,0 +1,39 @@
+//
+//  SetupTests.m
+//  myMusicStand
+//
+//  Created by Steve Solomon on 5/11/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "DelegateTests.h"
+
+
+@implementation DelegateTests
+
+- (void)setUp
+{
+    [super setUp];
+    yourApplicationDelegate = [[UIApplication sharedApplication] delegate];
+}
+
+- (void)tearDown
+{
+    [super tearDown];
+}
+- (void)testAppDelegate 
+{
+    STAssertNotNil(yourApplicationDelegate, @"UIApplication failed to find the AppDelegate");
+}
+
+- (void)testWindowSubview
+{
+    UIWindow *window = [yourApplicationDelegate window];
+    STAssertEquals(1, (NSInteger)[[window subviews] count], 
+                   @"The window shoud have 1 subview but it had %lu", [[window subviews] count]);
+    
+    UIView *subview;
+    STAssertNoThrow(subview = [[window subviews] objectAtIndex:0], 
+                    @"Accessing the subview should be valid");
+}
+@end
