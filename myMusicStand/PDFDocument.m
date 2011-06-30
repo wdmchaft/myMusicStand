4d0de814b7921aa89a9b7d232dc322e18c115a45
Steven Solomon <solomon.steven.m@gmail.com>
2011-06-30 12:10:41 -0400

    Add ability to open pdfs in PDFDocument

--- myMusicStand/PDFDocument.m
+++ myMusicStand/PDFDocument.m
@@ -7,17 +7,40 @@
 //
 
 #import "PDFDocument.h"
+#define DOCUMENT_HAS_OPENED YES
+#define DOCUMENT_FAILED_TO_OPEN NO
 
 @implementation PDFDocument
 
-- (id)init
+/*
+ *  Load the pdf document from the NSData we are passed in contents param
+ */
+- (BOOL)loadFromContents:(id)contents 
+                  ofType:(NSString *)typeName 
+                   error:(NSError **)outError
 {
-    self = [super init];
-    if (self) {
-        // Initialization code here.
+    
+    // create a data provider for pdf using NSData and toll free bridging
+    CGDataProviderRef provider = CGDataProviderCreateWithCFData((CFDataRef)contents);
+    // create pdf document from provider
+    document = CGPDFDocumentCreateWithProvider(provider);
+    CGDataProviderRelease(provider);
+    
+    // Check if we failed to open document
+    if (document == nil)
+    {
+        return DOCUMENT_FAILED_TO_OPEN;
     }
     
-    return self;
+    return DOCUMENT_HAS_OPENED;
+    
 }
+
+- (void)dealloc
+{
+    CGPDFDocumentRelease(document);
+    [super dealloc];
+}
+
 
 @end
